5877cca1dfb34f48e8951238e67612e72643ba13
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2023-01-07 10:21:23 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,22 +1,21 @@
 import './style.css';
 import { timer, take, map, tap, share } from 'rxjs';
 import { logToDiv } from './display';
-import { concatAll, mergeAll, switchAll, exhaustAll } from 'rxjs';
+import { concatMap, mergeMap, switchMap, exhaustMap } from 'rxjs';
 
 const source = timer(0, 2000).pipe(take(4));
 const countTill2Task = timer(300, 1500).pipe(take(3));
 
 //Shared source of mapped tasks for testing.
-const mapDataToTask = source.pipe(
+const sharedSource = source.pipe(
   tap((v) => logToDiv('one', v)),
-  map((v) => countTill2Task),
   share()
 );
 //Each data from source, maps to a countTill2Task
-mapDataToTask.pipe(concatAll()).subscribe((v) => logToDiv('two', v));
-mapDataToTask.pipe(mergeAll()).subscribe((v) => logToDiv('three', v));
-mapDataToTask.pipe(exhaustAll()).subscribe((v) => logToDiv('four', v));
-mapDataToTask.pipe(switchAll()).subscribe((v) => logToDiv('five', v));
+sharedSource.pipe(concatMap((v) => countTill2Task)).subscribe((v) => logToDiv('two', v));
+sharedSource.pipe(mergeMap((v) => countTill2Task)).subscribe((v) => logToDiv('three', v));
+sharedSource.pipe(exhaustMap((v) => countTill2Task)).subscribe((v) => logToDiv('four', v));
+sharedSource.pipe(switchMap((v) => countTill2Task)).subscribe((v) => logToDiv('five', v));
 
 //Displayed for reference purpose
 countTill2Task.subscribe((v) => logToDiv('map', v));
